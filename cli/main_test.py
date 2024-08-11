1e15b37ae5d585c8f6011d8756435a8f61f9a83e
SinMc <69413410+SinMc@users.noreply.github.com>
2024-08-11 08:04:33 +0000

    create new test

--- cli/main_test.py
+++ cli/main_test.py
@@ -1,11 +1,21 @@
+import os
 import pytest
 
-from cli.main import retrieve_locations, network_request
+from cli.main import retrieve_locations, network_request, main
+
+filepath = './cli/test/fixtures/test.csv'
+
+def test_main():
+    files = main(filepath)
+    for file in files:
+        assert os.path.isfile(file)
+
 
 def test_read_csv_file():
-    # given
-    filepath = './cli/test/fixtures/test.csv'
-    known_locations = [{'lat': -37.7974457951565, 'lon': 145.07593755525113}, {'lat': -37.790440, 'lon': 145.098495}]
+    known_locations = [
+        {'coordinates':{'latitude': -37.7974457951565, 'longitude': 145.07593755525113}},
+        {'coordinates':{'latitude': -37.790440, 'longitude': 145.098495}}
+    ]
     # when
     locations = retrieve_locations(filepath)
     # then
