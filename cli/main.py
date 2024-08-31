4142c2fd014b86296b0730a7f4129cd25888b661
SinMc <69413410+SinMc@users.noreply.github.com>
2024-08-31 09:01:53 +0000

    working maps + tidying up a bit

--- cli/main.py
+++ cli/main.py
@@ -2,26 +2,94 @@
 [changeme]
 """
 import os
+import folium.map
 import requests
 import sys
 import json
-import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+import folium
+from PIL import Image
+import io
+import selenium
 
 filepath = './cli/test/fixtures/test.csv'
 
 def main(input_path):
-    # pass
-    response_json = network_request()
+    locations = retrieve_locations(input_path)
+    response = network_request(locations)
+    w_temp(response)
+    graph(response)
+    csvw(response)
+    maps(locations)
+
+
+def w_temp(response_json):
     with open("coordinate.json", "w") as file: 
          file.write(json.dumps(response_json))
-    return ["coordinate.json"]
 
 
-def format_output():
+def reader():
     with open("./coordinate.json", "r") as f:
         data = json.load(f)
+    return data
+
+
+def maps(locations):
+    x, y = zip(*locations)
+    map = folium.Map(zoom_control=False)
+    map.fit_bounds([[min(x), min(y)], [max(x), max(y)]])
+
+    for location in locations:
+        folium.Marker(location=(location), icon=folium.Icon(icon="cloud")).add_to(map)
+    map_data = map._to_png()
+    img_map = Image.open(io.BytesIO(map_data))
+    img_map.save('Map.png')
+
+
+def csvw(data):
+    for loc in data:
+        d = loc["severities"]
+        name = str(loc["metadata"]["location"]["latitude"]) + ", " + str(loc["metadata"]["location"]["longitude"])
+
+        lst = []
+        header = []
+
+        for rcp in d:
+            r = d[rcp]
+            r = {k: v for k, v in r.items() if v !=None}
+
+            for hazard in r:
+                h = r[hazard]
+
+                file_name = name + '/' + rcp + f'/{hazard}_risk.csv'
+
+                with open(file_name, 'w', newline='') as f:
+                    writer = csv.writer(f)
+
+                    for likelihood in h:
+                        n = h[likelihood]
+                        header.clear()
+                        header.append("return period")
+                        lst.clear()
+                        lst.append(likelihood)
+
+                        for key, value in n.items():
+                            header.append(key)
+                            f.flush()                                
+                            lst.append(value)
+
+                        if os.path.getsize(file_name) == 0:
+                            writer.writerow(header)
+                            
+                        writer.writerow(lst)
+                
+
+
+
+def graph(data):
     for loc in data:
         newpath = str(loc["metadata"]["location"]["latitude"]) + ", " + str(loc["metadata"]["location"]["longitude"])
         if not os.path.exists(newpath):
@@ -30,16 +98,17 @@ def format_output():
         d = loc["severities"]
 
         for rcp in d:
-            # print(rcp)
             r = d[rcp]
             r = {k: v for k, v in r.items() if v !=None}
+
+            f_rcp = newpath + f"/{rcp}"
+            if not os.path.exists(f_rcp):
+                os.makedirs(f_rcp)
 
             fig, axs = plt.subplots(len(r), sharex= True)
 
             for i, hazard in enumerate(r):
-                # print(hazard)
                 h = r[hazard]
-                # print(h)
                 fig.suptitle("Hazards")
                 fig.supxlabel("Year", va= "center")
                 fig.supylabel("Risk")
@@ -48,47 +117,46 @@ def format_output():
                 axs[i].set_title(hazard)
                 fig.subplots_adjust(hspace=0.5)
 
-                for number in h:
-                    # print(number)
-                    n = h[number]
+                for likelihood in h:
+                    n = h[likelihood]
                     x = n.keys()
                     y = n.values()
                     axs[i].plot(x,y)
 
-                # print(counter)
             plt.xticks(rotation=30)
-            fig.savefig(newpath + f"/graph_{rcp}.png")
-
-    return ["graph.png"]
+            fig.savefig(f_rcp + "/graph.png")
 
 
 
 def retrieve_locations(filepath: str):
     with open(filepath, encoding="utf-8") as f:
-        lines = f.readlines()  # read the file
+        lines = f.readlines()
 
     locations = []
 
     for line in lines[1:]:
-        separatedcoords = line.split(",") # separates the coordinate values
-        lat = float(separatedcoords[0].strip())
-        lon = float(separatedcoords[1].strip())
-        n_coordinate = {"coordinates":{"latitude":lat,"longitude":lon}}
-        locations.append(n_coordinate)
+        line = line.strip()
+        ls = line.split(",") 
+        fls = float(ls[0]), float(ls[1])
+        locations.append(fls)
     return locations
 
 
-def network_request():
-    locations = retrieve_locations(filepath)
+def network_request(locations):
+    f_locations = []
+    response_json = []
+
     url = "https://api.climaterisk.qa/v1/severities"
     api_token = os.environ.get("SEVERITIES_API_TOKEN")
     headers = {"Authorization": "Basic {}".format(api_token)}
 
-    response_json = []
     for location in locations:
-        response = requests.post(url, data= json.dumps(location), headers= headers)
+        f_coord = {"coordinates":{"latitude":location[0],"longitude":location[1]}}
+        f_locations.append(f_coord)
+    
+    for f_location in f_locations:
+        response = requests.post(url, data= json.dumps(f_location), headers= headers)
         response_json.append(response.json())
-    # print(response_json)
     return response_json
 
 
